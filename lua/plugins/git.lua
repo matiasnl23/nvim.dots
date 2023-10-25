090ce65ff7d9b87af8ce66e9da0c73f561746759
Matías Lobo <matiasnl.1993@gmail.com>
2023-10-25 00:09:51 -0300

    Add some git maps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,15 +1,38 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {},
-    },
-    {
-        "tpope/vim-fugitive",
-        config = function ()
-            local map = require("helpers.keys").map
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+    config = function()
+      local gs = require("gitsigns")
 
-            map("n", "<leader>ga", "<cmd>Git add %<cr>", "Stage current file")
-            map("n", "<leader>gb", "<cmd>Git blame<cr>", "Show blame")
+      gs.setup({
+        on_attach = function ()
+          local map = require("helpers.keys").map
+
+          -- Navigation
+          map("n", "]c", function ()
+            if vim.wo.diff then return "]c" end
+            vim.schedule(function () gs.next_hunk() end)
+            return "<Ignore>"
+          end, "Go to next change")
+          map("n", "[c", function ()
+            if vim.wo.diff then return "[c" end
+            vim.schedule(function () gs.prev_hunk() end)
+            return "<Ignore>"
+          end, "Go to previous change")
+
+          map("n", "<leader>gS", gs.toggle_signs, "Toggle gitsigns")
         end
-    }
+      })
+    end
+  },
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      local map = require("helpers.keys").map
+
+      map("n", "<leader>ga", "<cmd>Git add %<cr>", "Stage current file")
+      map("n", "<leader>gb", "<cmd>Git blame<cr>", "Show blame")
+    end
+  }
 }
