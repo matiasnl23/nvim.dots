b549ed93e736a764c109f66dbabfd186e8d28aff
Matías Lobo <matiasnl.1993@gmail.com>
2024-01-31 01:04:19 -0300

    New surround lib

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -29,7 +29,6 @@ return {
             require("mini.pairs").setup()
         end
     },
-    "tpope/vim-surround",
     {
         "NMAC427/guess-indent.nvim",
         config = function()
@@ -57,12 +56,25 @@ return {
         "folke/flash.nvim",
         event = "VeryLazy",
         opts = {},
+        -- stylua: ignore
         keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "x", "o" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = "c", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-        }
+            { "s", mode = 'v', false },
+            { "S", mode = 'v', false },
+            -- { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            -- { "S",     mode  = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            -- { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+    {
+        'kylechui/nvim-surround',
+        version = '*', -- Use for stability; omit to use `main` branch for the latest features
+        event = 'VeryLazy',
+        config = function()
+            require('nvim-surround').setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
     }
 }
