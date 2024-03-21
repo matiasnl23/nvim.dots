bae89be5dae7674503b31addb97febd5fcdcf1bd
Matías Lobo <matiasnl.1993@gmail.com>
2024-03-20 22:22:22 -0300

    Fix neotest

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -2,7 +2,9 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
       "haydenmeade/neotest-jest",
       "marilari88/neotest-vitest",
     },
